6e8cdc2453d8556b39ed98a7f999b4256cab90da
andrewfstratton <andrewfstratton@gmail.com>
2025-04-24 10:59:30 +0100

    add simple function call example

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Op func()
+
+var ops map[string]Op
+
+func hi() {
+	fmt.Println("hi")
+}
+
+func hello(name string) Op {
+	return func() {
+		fmt.Printf("Hello %s!", name)
+	}
+}
+
+func init() {
+	ops = make(map[string]Op)
+	ops["hi"] = hi
+	ops["bob"] = hello("Bob")
+	ops["jill"] = hello("Jill")
+	ops["nl"] = func() { fmt.Println() }
+}
+
+func parse(line string) {
+	for _, word := range strings.Fields(line) {
+		op, ok := ops[word]
+		if ok {
+			op()
+		}
+	}
+}
+
+func main() {
+	parse("hi bob nl jill nl")
+}
