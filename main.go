4b9e7c4db6cdf7e1f2e8db5e280e6d8c1cfb918c
andrewfstratton <andrewfstratton@gmail.com>
2025-04-24 15:18:44 +0100

    add AddOp and initial parseCall

--- main.go
+++ main.go
@@ -1,43 +1,71 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 )
 
-type Op func()
+type Op func() string
 
 var ops map[string]Op
 
-func hi() {
-	fmt.Println("hi")
-}
-
 func hello(name string) Op {
-	return func() {
-		fmt.Printf("Hello %s!", name)
+	return func() string {
+		return "Hello " + name + "!"
 	}
 }
 
 func init() {
 	ops = make(map[string]Op)
-	ops["hi"] = hi
-	ops["bob"] = hello("Bob")
-	ops["jill"] = hello("Jill")
-	ops["nl"] = func() { fmt.Println() }
+	addOp("bob", hello("Bob"))
+	addOp("jill", hello("Jill"))
 }
 
-func parse(line string) (txt string, ok bool) {
-	var result string
-	for _, word := range strings.Fields(line) {
-		op, ok := ops[word]
+func parseCall(call string) (Op, error) {
+	var result Op
+	var err error
+	if call != "" {
+		op, ok := ops[call]
 		if ok {
-			op()
+			result = op
+		} else {
+			err = errors.New("Failed to parse call '" + call + "'")
 		}
 	}
-	return result, false
+	return result, err
+}
+
+func parseLine(line string) (string, error) {
+	var result string
+	var err error
+	for _, word := range strings.Fields(line) {
+		var op Op
+		op, err = parseCall(word)
+		if err == nil {
+			result += op()
+		} else {
+			break // i.e. bail out early
+		}
+	}
+	return result, err
+}
+
+func addOp(lookup string, op Op) (err error) {
+	if lookup == "" {
+		err = errors.New("ignoring new operation with empty lookup")
+	} else if op == nil {
+		err = errors.New("ignoring nil operation for '" + lookup + "'")
+	} else {
+		ops[lookup] = op
+	}
+	return err
 }
 
 func main() {
-	parse("hi bob nl jill nl")
+	result, err := parseLine("hi bob nl jill nl")
+	if err != nil {
+		fmt.Println("ERR:", err)
+	}
+	fmt.Println(result)
 }
--- parse_test.go
+++ parse_test.go
@@ -6,8 +6,32 @@ import (
 	"github.com/andrewfstratton/quandoscript/assert"
 )
 
+func TestAddEmptyStringWordOp(t *testing.T) {
+	err := addOp("", func() string { return "Should never allow map to empty string" })
+	assert.Neq(t, err, nil)
+}
+
 func TestParse(t *testing.T) {
-	result, ok := parse("")
-	assert.Eq(t, result, "")
-	assert.Eq(t, ok, true)
+	op, err := parseCall("")
+	assert.Eq(t, err, nil)
+	assert.True(t, op == nil)
+}
+
+func TestParseMissing(t *testing.T) {
+	op, err := parseCall("_")
+	assert.Neq(t, err, nil)
+	assert.True(t, op == nil)
+}
+
+func TestParseWhiteSpace(t *testing.T) {
+	txt, err := parseLine(" ")
+	assert.Eq(t, err, nil)
+	assert.Eq(t, txt, "")
+}
+
+func init() {
+	// first below shouldn't be added
+	addOp("", func() string { return "Should never allow map to empty string" })
+	addOp("hi", func() string { return "hi" })
+	addOp("nl", func() string { return "\n" })
 }
