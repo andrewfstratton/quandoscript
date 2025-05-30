29b6d9251a1272309962e599d313dbbdf5088472
andrewfstratton <andrewfstratton@gmail.com>
2025-05-30 13:55:40 +0100

    add example run

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/andrewfstratton/quandoscript/block/widget/stringinput"
+	"github.com/andrewfstratton/quandoscript/block/widget/text"
+	"github.com/andrewfstratton/quandoscript/library"
+	"github.com/andrewfstratton/quandoscript/run/param"
+)
+
+func init() {
+	greeting := stringinput.New("greeting").Empty("greeting")
+	txt := stringinput.New("txt").Empty("greeting")
+
+	library.NewBlockType("server.log", "misc", op).Add(
+		text.New("Log "),
+		greeting,
+		text.New(" "),
+		txt)
+}
+
+func op(outer param.Params) func(param.Params) {
+	greeting := param.StringParam{Lookup: "greeting", Val: ""}
+	greeting.Update(outer)
+	txt := param.StringParam{Lookup: "txt", Val: ""}
+	return func(inner param.Params) {
+		txt.Update(inner)
+		now := time.Now()
+		fmt.Println("Log:", now, greeting.Val, txt.Val)
+	}
+}
+
+func main() {
+	bt, _ := library.FindBlockType("server.log")
+	fmt.Println(bt)
+	fmt.Println(bt.Replace("{{.Widgets}}"))
+}
