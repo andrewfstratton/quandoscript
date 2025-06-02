ae51f8715f1f421c3babff8485c50919c3a887e6
andrewfstratton <andrewfstratton@gmail.com>
2025-06-02 23:31:30 +0100

    add numberparam

--- action/param/param.go
+++ action/param/param.go
@@ -24,11 +24,45 @@ type StringParam struct {
 	Val    string
 }
 
+type NumberParam struct {
+	Lookup string
+	Val    float64
+}
+
+type IdParam struct {
+	Lookup string
+	Val    int
+}
+
 func (param *StringParam) Update(params Params) {
 	p, found := params[param.Lookup]
 	if found {
 		if p.Qtype == STRING {
 			param.Val = p.Val.(string)
+			return
+		}
+		// lookup variable here...
+		fmt.Println("Error : ", param.Lookup, " incorrect type")
+	}
+}
+
+func (param *NumberParam) Update(params Params) {
+	p, found := params[param.Lookup]
+	if found {
+		if p.Qtype == NUMBER {
+			param.Val = p.Val.(float64)
+			return
+		}
+		// lookup variable here...
+		fmt.Println("Error : ", param.Lookup, " incorrect type")
+	}
+}
+
+func (param *IdParam) Update(params Params) {
+	p, found := params[param.Lookup]
+	if found {
+		if p.Qtype == LINEID {
+			param.Val = p.Val.(int)
 			return
 		}
 		// lookup variable here...
