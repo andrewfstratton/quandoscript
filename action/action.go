cd4e2769424d297f6f07ef7a38991058561d9826
andrewfstratton <andrewfstratton@gmail.com>
2025-06-03 16:22:28 +0100

    add sequences (groups) of actions inc. example after callback

--- action/action.go
+++ action/action.go
@@ -7,23 +7,41 @@ import (
 type Action struct {
 	op     func(param.Params)
 	Params param.Params
+	NextId int
 	// context
 }
 
-var Actions map[int]*Action
-var IdOrdered []int
+var Actions map[int]*Action // map id to action
+var last *Action
+var StartId int = -1
 
 type Op func(param.Params)
 type OpOp func(param.Params) func(param.Params)
 
 func New(o Op, late param.Params) *Action {
-	return &Action{op: o, Params: late}
+	action := Action{op: o, Params: late, NextId: -1} // N.B. -1 is to show no following action
+	return &action
 }
 
 func (action *Action) Exec() {
 	action.op(action.Params)
 }
 
+func NewGroup() {
+	last = nil // so we don't append to the same group
+}
+
+func Add(id int, action *Action) {
+	if StartId == -1 {
+		StartId = id
+	}
+	Actions[id] = action
+	if last != nil {
+		last.NextId = id
+	}
+	last = action
+}
+
 func init() {
-	Actions = map[int]*Action{}
+	Actions = map[int]*Action{} // i.e. the lookup table to find any action
 }
--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"strconv"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/andrewfstratton/quandoscript/action"
 	"github.com/andrewfstratton/quandoscript/action/param"
+	"github.com/andrewfstratton/quandoscript/block/widget/numberinput"
 	"github.com/andrewfstratton/quandoscript/block/widget/stringinput"
 	"github.com/andrewfstratton/quandoscript/block/widget/text"
 	"github.com/andrewfstratton/quandoscript/library"
@@ -39,36 +40,91 @@ func logop(outer param.Params) func(param.Params) {
 	}
 }
 
+func init_after() {
+	seconds := numberinput.New("secs").Empty("seconds").Min(0).Max(999).Width(4).Default(1)
+	callback := numberinput.New("callback").Empty("callback").Min(0).Max(999).Width(4).Default(999)
+
+	library.NewBlockType("system.after", "misc", timeop).Add(
+		text.New("After "),
+		seconds,
+		text.New("secs"),
+		callback,
+	)
+}
+
+func timeop(outer param.Params) func(param.Params) {
+	secs := param.NumberParam{Lookup: "secs", Val: 1.0}
+	secs.Update(outer)
+	callback := param.IdParam{Lookup: "callback", Val: 0}
+	callback.Update(outer)
+	return func(inner param.Params) {
+		secs.Update(inner)
+		callback.Update(inner)
+		// fmt.Println("After:", secs.Val, "secs, callback:", callback)
+		isecs := int(secs.Val)
+		time.AfterFunc(time.Duration(isecs)*time.Second, func() {
+			runGroup(callback.Val)
+		})
+	}
+}
+
 func init() {
 	init_log()
+	init_after()
 }
 
 func parseLines(in string) {
 	scanner := bufio.NewScanner(strings.NewReader(in))
+	new_group := true
 	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			// fmt.Println("End of main block")
+			new_group = true
+			continue
+		}
 		lineid, word, params, err := parse.Line(scanner.Text())
-		fmt.Println(lineid, word, params, err)
+		if err != nil {
+			fmt.Println(lineid, word, params, err)
+		}
 		o := library.NewAction(word, params, nil)
-		action.IdOrdered = append(action.IdOrdered, lineid)
-		action.Actions[lineid] = o
+		if new_group {
+			action.NewGroup()
+			new_group = false
+		}
+		action.Add(lineid, o)
 	}
 }
 
 const (
 	TEST_LINES = `0 system.log(greeting"Hi",txt"Bob")
-1 system.log(greeting"Hello",txt"Jane")
-2 system.log(greeting"Hello",txt"Jane")
+1 system.after(secs#2,callback:3)
+
 3 system.log(greeting"Hello",txt"Jane")
-4 system.log(greeting"Hello",txt"Jane")
-5 system.log(greeting"Hello",txt"Jane")
-6 system.log(greeting"Hello",txt"Jane")
-7 system.log(greeting"Hello",txt"Jane")`
+4 system.log(greeting"Bye",txt"Bob")
+5 system.after(secs#2,callback:6)
+
+6 system.log(greeting"Bye",txt"Jane")
+`
 )
+
+func runGroup(id int) {
+	if id == -1 { // start at top
+		fmt.Println("No actions found, exiting...")
+		os.Exit(0)
+	}
+
+	for id != -1 {
+		// fmt.Print(strconv.Itoa(id) + "-")
+		act := action.Actions[id]
+		act.Exec()
+		id = act.NextId
+	}
+
+}
 
 func main() {
 	parseLines(TEST_LINES)
-	for l := range action.IdOrdered {
-		fmt.Print(strconv.Itoa(l) + "-")
-		action.Actions[l].Exec()
-	}
+	runGroup(action.StartId)
+	time.Sleep(10 * time.Second)
 }
