37dff9cc426f44c5057aca9467212abe4075d32c
andrewfstratton <andrewfstratton@gmail.com>
2025-06-13 13:14:45 +0100

    add every sample and small tidy of example

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ type AfterDefn struct {
 	Class    struct{}                `_:"misc"`
 	_        text.Text               `txt:"After " iconify:"true"`
 	Secs     numberinput.NumberInput `empty:"seconds" min:"0" max:"999" width:"4" default:"1"` // min:0 max:999 width:4 default:1`
-	_        text.Text               `txt:" "`
+	_        text.Text               `txt:"seconds"`
 	Callback idinput.IdInput
 }
 
@@ -57,27 +57,57 @@ func init_after() {
 			return func(late param.Params) {
 				secs.Update(late)
 				callback.Update(late)
-				time.AfterFunc(time.Duration(secs.Int())*time.Second, func() {
+				time.AfterFunc(secs.Duration(), func() {
 					action.Run(callback.Val)
 				})
 			}
 		})
 }
 
+type EveryDefn struct {
+	TypeName struct{}                `_:"system.every"`
+	Class    struct{}                `_:"misc"`
+	_        text.Text               `txt:"Every " iconify:"true"`
+	Secs     numberinput.NumberInput `empty:"seconds" min:"0" max:"999" width:"4" default:"1"`
+	_        text.Text               `txt:"seconds"`
+	Callback idinput.IdInput
+}
+
+func init_every() {
+	every := &EveryDefn{}
+	block.New(every).Op(
+		func(early param.Params) func(param.Params) {
+			secs := every.Secs.Param(early)
+			callback := every.Callback.Param(early)
+			return func(late param.Params) {
+				secs.Update(late)
+				callback.Update(late)
+				go func() {
+					for range time.Tick(secs.Duration()) {
+						action.Run(callback.Val)
+					}
+				}()
+			}
+		})
+}
 func init() {
 	init_log()
 	init_after()
+	init_every()
 }
 
 const (
 	TEST_LINES = `0 system.log(greeting"Hi",txt"Bob")
 1 system.after(secs#2,callback:3)
+2 system.every(secs#0.5,callback:7)
 
 3 system.log(greeting"Hello",txt"Jane")
 4 system.log(greeting"Bye",txt"Bob")
-5 system.after(secs#2,callback:6)
+5 system.after(secs#0.9,callback:6)
 
 6 system.log(greeting"Bye",txt"Jane")
+
+7 system.log(txt".")
 `
 )
 
