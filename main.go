3b093a579aeacd2d8fa52e69f32d8a60244624b5
andrewfstratton <andrewfstratton@gmail.com>
2025-06-23 15:31:25 +0100

    add different example to show blocks can callback blocks that callback blocks...

--- main.go
+++ main.go
@@ -38,12 +38,12 @@ func init_log() { // sets up the Block (UI) ONLY  - doesn't setup any action eve
 }
 
 type Defn struct {
-	TypeName struct{}                `_:"system.after"`
+	TypeName struct{}                `_:"time.after"`
 	Class    struct{}                `_:"misc"`
 	_        text.Text               `txt:"After " iconify:"true"`
 	Secs     numberinput.NumberInput `empty:"seconds" min:"0" max:"999" width:"4" default:"1"` // min:0 max:999 width:4 default:1`
 	_        text.Text               `txt:"seconds"`
-	Callback boxinput.BoxInput       `txt:"box"`
+	Box      boxinput.BoxInput
 }
 
 func init_after() {
@@ -51,7 +51,7 @@ func init_after() {
 	library.Block(defn).Op(
 		func(early param.Params) func(param.Params) {
 			secs := defn.Secs.Param(early)
-			callback := defn.Callback.Param(early)
+			callback := defn.Box.Param(early)
 			return func(late param.Params) {
 				secs.Update(late)
 				callback.Update(late)
@@ -63,7 +63,7 @@ func init_after() {
 }
 
 type Every struct {
-	TypeName struct{}                `_:"system.every"`
+	TypeName struct{}                `_:"time.every"`
 	Class    struct{}                `_:"misc"`
 	_        text.Text               `txt:"Every " iconify:"true"`
 	Secs     numberinput.NumberInput `empty:"seconds" min:"0" max:"999" width:"4" default:"1"`
@@ -96,17 +96,19 @@ func init() {
 }
 
 const (
-	TEST_LINES = `0 system.log(Greeting"Hi",Txt"Bob")
-1 system.after(Secs#2,Callback:3)
-2 system.every(Secs#0.5,Callback:7)
+	TEST_LINES = `1 system.log(Greeting"1",Txt"1")
+2 system.log(Greeting"2",Txt"2")
+3 system.log(Greeting"3",Txt"3")
+4 time.after(Secs#4,Box:5)
+6 time.after(Secs#6,Box:0)
+7 time.after(Secs#7,Box:8)
 
-3 system.log(Greeting"Hello",Txt"Jane")
-4 system.log(Greeting"Bye",Txt"Bob")
-5 system.after(Secs#0.9,Callback:6)
+5 system.log(Greeting"5",Txt"5")
 
-6 system.log(Greeting"Bye",Txt"Jane")
+8 system.log(Greeting"8",Txt"8")
+9 time.after(Secs#9,Box:10)
 
-7 system.log(Txt".")
+10 system.log(Greeting"10",Txt"10")
 `
 )
 
@@ -116,5 +118,5 @@ func main() {
 	if warn != "" {
 		fmt.Println(warn)
 	}
-	time.Sleep(10 * time.Second)
+	time.Sleep(20 * time.Second)
 }
